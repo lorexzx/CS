7d57f0f4cf8abe1df749a2f64fc5fd37360ac49e
maxg0072 <143296009+maxg0072@users.noreply.github.com>
2023-12-13 18:23:16 +0100

    d

--- MVP2.py
+++ MVP2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.graph_objects as go
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
