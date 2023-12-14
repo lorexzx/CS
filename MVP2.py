4f6302b48ac5a3be52467d3066dec468c2a358e3
maxg0072 <143296009+maxg0072@users.noreply.github.com>
2023-12-14 16:35:53 +0100

    x

--- MVP2.py
+++ MVP2.py
@@ -280,7 +280,7 @@ def display_property_details(row):
         st.markdown('</div>', unsafe_allow_html=True)
 
 
-def render_step(step, placeholder):
+def render_step(step, placeholder, sorted_data):
     with placeholder.container():
         if step == 0:
             # Step 1: Location
@@ -356,7 +356,7 @@ def render_step(step, placeholder):
                     extracted_zip_code = st.session_state.extracted_zip_code
                     if extracted_zip_code is not None:
                         #predicted_price = predict_price(st.session_state.size_m2, extracted_zip_code, st.session_state.rooms, model) krish
-                        predicted_price = predict_price(st.session_state.size_m2, extracted_zip_code, st.session_state.rooms, model)
+                        predicted_price = predict_price(st.session_state.size_m2, extracted_zip_code, st.session_state.rooms, model, sorted_data)
                         if predicted_price is not None:
                             st.session_state.predicted_price = predicted_price  # Speichern des berechneten Preises im session state
                             st.markdown(f"**The predicted price for the apartment is CHF {predicted_price:.2f}**", unsafe_allow_html=True)
