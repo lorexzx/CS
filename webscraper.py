e58e8da9ec567a244ea72b624c588be7131e2761
maxg0072 <143296009+maxg0072@users.noreply.github.com>
2023-12-14 20:01:07 +0100

    xxx

--- webscraper.py
+++ webscraper.py
@@ -1,3 +1,28 @@
+"""
+Web Scraper for Real Estate Data from RealAdvisor.ch
+
+This script is a web scraper designed to extract real estate listings from the website realadvisor.ch. 
+It specifically targets properties in the St. Gallen area. The scraper navigates through the site, 
+collects vital information such as broker names, descriptions, locations, prices, price per square meter, zip codes, 
+and website links. 
+
+The collected data is cleaned and structured using custom functions. This structured data is then compiled 
+into a pandas DataFrame. Each page's data from the website is appended to a collective DataFrame, 
+which is finally saved as an Excel file. This file serves as a dataset for further analysis, such as 
+building a linear regression model to understand and predict real estate price trends.
+
+Usage:
+- The script is currently set to scrape a limited number of pages (adjustable in the for loop).
+- The required libraries (pandas, BeautifulSoup, requests) should be installed before running the script.
+- The final dataset is saved as an Excel file on the specified path.
+
+Note: Web scraping is subject to the terms of use of the website. Ensure compliance with realadvisor.ch's 
+terms and conditions before running the scraper.
+"""
+
+
+
+
 import re
 import pandas as pd
 from bs4 import BeautifulSoup
@@ -5,38 +30,40 @@ import requests
 from datetime import datetime
 
 def clean_price(price):
-    # Ersetzt Nicht-Brechende Leerzeichen und entfernt "-Zeichen
+    # Replace non-breaking spaces and remove hyphens from price
     return price.replace('\xa0', ' ').replace('-', '').strip()
 
 def clean_pricem2(pricem2):
-    # Entfernt Nicht-Brechende Leerzeichen und alles nach "/ m²"
+    # Remove non-breaking spaces and anything after "/ m²" in price per square meter
     pricem2 = pricem2.replace('\xa0', ' ')
     return re.split(r' / m²', pricem2)[0].strip()
 
-# Basis-URL und gemeinsame Parameter
+# Base URL and common parameters for scraping
 base_url = 'https://realadvisor.ch/de/mieten/9000-st-gallen/haus-wohnung'
 params = '?east=9.651524100294182&north=47.78651906423726&south=47.05433631754212&west=9.091221365919182'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 all_data = []
 
-# Schleife über die Seitenzahlen
-for page in range(1,2):  # Ändern Sie 5 auf die tatsächliche Anzahl der Seiten + 1
+# Loop over page numbers
+for page in range(1, 19):  # Scraping 18 pages
     if page == 1:
-        url = base_url + params  # URL für die erste Seite
+        url = base_url + params  # URL for the first page
     else:
-        url = f'{base_url}/seite-{page}{params}'  # URL für die anderen Seiten
+        url = f'{base_url}/seite-{page}{params}'  # URL for subsequent pages
 
-    # Anfrage an die Seite senden
+    # Send request to the page
     response = requests.get(url, headers=headers)
     
-    # Falls es sich um die zweite Seite handelt, die Anfrage nochmal senden
+    # If it's the second page, resend the request
     if page == 2:
         response = requests.get(url, headers=headers)
 
+    # If the response is successful, parse the page
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'lxml')
         
+        # Extracting various data from the page
         names_brooker_elements = soup.find_all('div', class_='css-1mtsvd6-AggregatesListingCard')
         names_brooker = [element.text.strip() for element in names_brooker_elements]
 
@@ -61,6 +88,7 @@ for page in range(1,2):  # Ändern Sie 5 auf die tatsächliche Anzahl der Seiten
         website_elements = soup.find_all('div', class_='css-vc4s6w-AggregatesListingCard')
         websites = [element.text.strip() for element in website_elements]
 
+        # Compiling page data into a dictionary
         page_data = {
             'Name': names_brooker,
             'Description': descriptions,
@@ -69,16 +97,16 @@ for page in range(1,2):  # Ändern Sie 5 auf die tatsächliche Anzahl der Seiten
             'Price': prices,
             'Price_per_m2_per_year': pricem2,
             'zip': zips,
-            'websiten': websites 
+            'Websites': websites 
         }
 
+        # Appending the data of each page to all_data list
         all_data.append(pd.DataFrame(page_data))
 
     else:
-        print(f"Fehler beim Laden der Seite {page}")
+        print(f"Error loading page {page}")
 
-# Zusammenführen aller Daten in einem DataFrame
-# Zusammenführen aller Daten in einem DataFrame
+# Merging all data into a single DataFrame
 df = pd.concat(all_data, ignore_index=True)
 
 #Save file in relative path
